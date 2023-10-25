9a1d8af70230a118eb5f6511423d12532fac51d2
Wietse Boersma <boersmawietse@gmail.com>
2023-10-25 14:47:40 +0200

    v3

--- python.py
+++ python.py
@@ -30,5 +30,4 @@ def birthday_greeting():
     return '<pre>' + output + '</pre>'
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
