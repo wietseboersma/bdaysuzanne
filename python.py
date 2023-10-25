7de1b06c056b2676484fc25995860f5f8c06fdbb
Wietse Boersma <boersmawietse@gmail.com>
2023-10-25 14:44:47 +0200

    v2

--- python.py
+++ python.py
@@ -1,22 +1,34 @@
+from flask import Flask
 import time
 import random
 
-zinnen = [
-    'Hieperdepiep?',
-    'Hoera!',
-    'Hieperdepiep?',
-    'Hoera!',
-    'Hieperdepiep?',
-    'Hoeraaaaaaa!',
-    'Een fijne verjaardag gewenst -turend over de velden met een whisky in je hand!'
-]
+app = Flask(__name__)
 
-kleuren = ['\033[91m', '\033[92m', '\033[93m', '\033[94m', '\033[95m', '\033[96m']  
-reset_kleur = '\033[0m'  
+@app.route('/')
+def birthday_greeting():
+    zinnen = [
+        'Hieperdepiep?',
+        'Hoera!',
+        'Hieperdepiep?',
+        'Hoera!',
+        'Hieperdepiep?',
+        'Hoeraaaaaaa!',
+        'Een fijne verjaardag gewenst -turend over de velden met een whisky in je hand!'
+    ]
 
-for zin in zinnen:
-    for char in zin:  
-        random_color = random.choice(kleuren)
-        print(random_color + char + reset_kleur, end='', flush=True)
-        time.sleep(0.1)  
-    print("\n")  
+    kleuren = ['\033[91m', '\033[92m', '\033[93m', '\033[94m', '\033[95m', '\033[96m']  
+    reset_kleur = '\033[0m'  
+
+    output = ""
+    for zin in zinnen:
+        for char in zin:  
+            random_color = random.choice(kleuren)
+            output += random_color + char + reset_kleur
+            time.sleep(0.1)  
+        output += "\n"
+    
+    return '<pre>' + output + '</pre>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
